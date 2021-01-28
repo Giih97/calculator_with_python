5348fdac3c953259f439badf5147887f0c785d63
Giih97 <gbamdev9@gmail.com>
2021-01-28 19:03:53 -0300

    ajustes

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder="./src/views")
 
+
 @app.route("/", methods=["GET", "POST"])
 def home():
     if (request.method == "GET"):
@@ -12,17 +13,17 @@ def home():
             num2 = request.form["num2"]
 
             if (request.form["opc"] == "soma"):
-                 soma = int(num1) + int(num2)
-                 return {
-                     "Resultado": str(soma)  #para exibir tem que transforma em texto
-                 } 
+                soma = int(num1) + int(num2)
+                return {
+                    # para exibir tem que transforma em texto
+                    "Resultado": str(soma)
+                }
 
             elif (request.form["opc"] == "sub"):
                 sub = int(num1) - int(num2)
                 return {
                     "Resultado": str(sub)
                 }
-
 
             elif (request.form["opc"] == "divisao"):
                 divisao = int(num1) // int(num2)
@@ -32,17 +33,17 @@ def home():
 
             else:
                 mult = int(num1) * int(num2)
-                return { 
+                return {
                     "Resultado": str(mult)
                 }
-
 
         else:
             return "informe um valor válido!"
 
-#@app.route("/<int:id>")
-#def Home_id(id):
+# @app.route("/<int:id>")
+# def Home_id(id):
 #   return str(id + 1)
+
 
 @app.errorhandler(404)
 def not_found(error):
